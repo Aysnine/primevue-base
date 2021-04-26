6bc5c28365ef63c0faade95e56d85dd9763b7f0f
CNine <cnine229@gmail.com>
2021-04-26 21:52:12 +0800

    chore: setup windicss

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,6 @@ import PrimeVue from 'primevue/config'
 import 'primevue/resources/themes/saga-blue/theme.css'
 import 'primevue/resources/primevue.min.css'
 import 'primeicons/primeicons.css'
+import 'virtual:windi.css'
 
 createApp(App).use(PrimeVue).mount('#app')
