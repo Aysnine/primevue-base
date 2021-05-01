085d96cd6760b1facd7145578aea847904b327c9
CNine <cnine229@gmail.com>
2021-05-01 23:35:18 +0800

    style: lint

--- typings/shims-vue.d.ts
+++ typings/shims-vue.d.ts
@@ -2,7 +2,7 @@ declare module '*.vue' {
   import Vue, { DefineComponent } from 'vue'
   // eslint-disable-next-line @typescript-eslint/ban-types, @typescript-eslint/no-explicit-any
   const component: DefineComponent<{}, {}, any>
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
   export type PluginFunction<T> = (app: Vue.App, ...options: any[]) => any
   export default component
 }
