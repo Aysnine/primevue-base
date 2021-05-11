5636b07421dd60813243c312f855dff6f3b741c0
CNine <cnine229@gmail.com>
2021-05-11 23:48:14 +0800

    feat: basic validate with zod

--- src/views/admin/student-create/step-personal.vue
+++ src/views/admin/student-create/step-personal.vue
@@ -1,35 +1,41 @@
 <template>
-  <PrimeCard>
-    <template #subtitle>Personal Information</template>
-    <template #content>
-      <div class="m-auto p-fluid max-w-100">
-        <div class="p-field">
-          <label>Name</label>
-          <PrimeInputText v-model.trim="name" type="text" />
+  <form autocomplete="off" @submit="submit">
+    <PrimeCard>
+      <template #subtitle>Personal Information</template>
+      <template #content>
+        <div class="m-auto p-fluid max-w-100">
+          <div class="p-field">
+            <label>Name</label>
+            <PrimeInputText
+              v-model.trim="name"
+              type="text"
+              :class="{ 'p-invalid': errors.name }"
+            />
+            <span v-if="errors.name" class="p-error">{{ errors.name }}</span>
+          </div>
+          <div class="p-field">
+            <label>Gender</label>
+            <PrimeDropdown
+              v-model="gender.value.value"
+              :options="genderOptions"
+              option-label="name"
+              option-value="code"
+            />
+          </div>
         </div>
-        <div class="p-field">
-          <label>Gender</label>
-          <PrimeDropdown
-            v-model="gender"
-            :options="genderOptions"
-            option-label="name"
-            option-value="code"
+      </template>
+      <template #footer>
+        <div class="flex justify-end">
+          <PrimeButton
+            type="submit"
+            label="Next"
+            icon="pi pi-angle-right"
+            icon-pos="right"
           />
         </div>
-      </div>
-    </template>
-    <template #footer>
-      <div class="flex justify-end">
-        <PrimeButton
-          type="button"
-          label="Next"
-          icon="pi pi-angle-right"
-          icon-pos="right"
-          @click="handleNext"
-        />
-      </div>
-    </template>
-  </PrimeCard>
+      </template>
+    </PrimeCard>
+  </form>
 </template>
 
 <script lang="ts">
@@ -39,6 +45,9 @@ import PrimeButton from 'primevue/button'
 import PrimeInputText from 'primevue/inputtext'
 import PrimeDropdown from 'primevue/dropdown'
 import { GENDER_TEXT } from '../../../constants'
+import { useField, useForm } from 'vee-validate'
+import { toFormValidator } from '@vee-validate/zod'
+import * as zod from 'zod'
 
 const stepIndex = 0
 
@@ -51,18 +60,28 @@ export default defineComponent({
   },
   emits: ['nextStep'],
   setup() {
-    const name = ref('')
-    const gender = ref(null)
+    const { handleSubmit, errors } = useForm({
+      validationSchema: toFormValidator(
+        zod.object({
+          name: zod.string().nonempty('This is required')
+        })
+      )
+    })
+
+    const { value: name } = useField('name', undefined, { initialValue: '' })
+    const gender = useField('name', undefined, { initialValue: '' })
+
+    const submit = handleSubmit((values) => {
+      console.log(values)
+
+      this.$emit('nextStep', { stepIndex, formData: { xxx: 1 } })
+    })
+
     const genderOptions = ref(
       Object.entries(GENDER_TEXT).map(([code, name]) => ({ code, name }))
     )
 
-    return { name, gender, genderOptions }
-  },
-  methods: {
-    handleNext() {
-      this.$emit('nextStep', { stepIndex, formData: { xxx: 1 } })
-    }
+    return { name, gender, errors, submit, genderOptions }
   }
 })
 </script>
